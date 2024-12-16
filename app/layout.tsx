c34b54e18d30cd6fa7ce6d668d0ade9cde6329f0
j <anonymosmail@gmail.com>
2024-12-16 18:03:36 +0100

    refactor app structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Wallet } from "@/lib/Wallet";
+import { Wallet } from "@/lib/wallet/Wallet";
 import Link from "next/link";
 
 
