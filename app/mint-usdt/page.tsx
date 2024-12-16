c8391710517f8dc52a933a7f787620ba7844a84f
j <anonymosmail@gmail.com>
2024-12-16 18:53:37 +0100

    add mint page

--- app/mint-usdt/page.tsx
+++ app/mint-usdt/page.tsx
@@ -1,9 +1,25 @@
+"use client";
 
+import { CryptoInput } from "@/lib/components/CryptoInput";
+import { useState } from "react";
+const MAX_MINT = 10000;
 export default function Home() {
-
+  const [value, setValue] = useState<number>(0);
+  function onSubmit (){
+    console.log("minting tokens", value)
+  }
   return (
     <div>
-      mint usdt page content
+      <span>Mint usdt to test our project :)</span>
+      <CryptoInput 
+        max={MAX_MINT}
+        maxMessage={`${MAX_MINT} maxium`}
+        tokenLogo={<></>}
+        initialValue={value}
+        onChange={setValue}
+        tokenName=""
+      />
+      <button onClick={onSubmit}>Mint</button>
     </div>
   );
 }
