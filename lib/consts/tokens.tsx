c352689762b58428200d7fd338c086be80361f7f
j <anonymosmail@gmail.com>
2024-12-16 18:52:56 +0100

    add tokens consts

---
+++ lib/consts/tokens.tsx
@@ -0,0 +1,19 @@
+type Token = {
+  name: string,
+  address: string
+}
+
+export const USDT: Token = Object.freeze({
+  name: "USDT",
+  address: ""
+})
+
+export const SPHERE: Token = Object.freeze({
+  name: "SPHERE",
+  address: ""
+})
+
+export const SPHERE_100: Token = Object.freeze({
+  name: "USDT",
+  address: ""
+})
