4da79baf30d6021952fe8b52454bd048723922e1
j <anonymosmail@gmail.com>
2024-12-17 13:01:43 +0100

    clean transactions code

--- lib/utils/transactions.tsx
+++ lib/utils/transactions.tsx
@@ -5,10 +5,10 @@ import HEDERA_DATA from '@/hedera_data.json'
 
 const SPHERE_WALLET_ADRESS = HEDERA_DATA.creatorAccount.accountId;
 const SUCCESS_MESSAGE = "SUCCESS";
-const OPERATOR_ID = "0.0.5274980"; 
+const OPERATOR_ID = "0.0.5274980";
 const OPERATOR_KEY = PrivateKey.fromString("302e020100300506032b6570042204208d9ddfcb9c80cb6f2181c07b44ebed3bfdadb051eadc80b3f94fcf65d629be5e");
 export async function mintUsdt(rawAmount: number) {
-  if(rawAmount <= 0) return;
+  if (rawAmount <= 0) return;
   const accountIdString = useWallet.getState().accountId;
   if (!accountIdString) return false;
   const accountId = AccountId.fromString(accountIdString);
@@ -19,38 +19,12 @@ export async function mintUsdt(rawAmount: number) {
 
   console.log("minting tokens: ", amount)
   // mint tokens
-
-  const client = await getClient();
-  const mintTx = await new TokenMintTransaction()
-    .setTokenId(USDT.address)
-    .setAmount(amount)
-    .setMaxTransactionFee(new Hbar(20)) //Use when HBAR is under 10 cents
-    .execute(client);
-
-
-  //Request the receipt of the transaction
-  const receipt = await mintTx.getReceipt(client);
-
-  //Get the transaction consensus status
-  const transactionStatus = receipt.status;
-
-  console.log("Transaction: " + mintTx.toString());
-  console.log("The transaction mint status " + transactionStatus.toString());
-  console.log("Transfering tokens from: " + SPHERE_WALLET_ADRESS + " to: " + accountId);
-  // Transfer minted tokens
-  const transferTx = await new TransferTransaction()
-    .addTokenTransfer(USDT.address, SPHERE_WALLET_ADRESS, -amount)
-    .addTokenTransfer(USDT.address, accountId, amount)
-    .execute(client);
-
-  //Request the receipt of the transaction
-  const receiptTransfer = await transferTx.getReceipt(client);
-
-  //Get the transaction consensus status
-  const transactionStatusTransfer = receiptTransfer.status;
-
-  console.log("Transaction: " + transferTx.toString());
-  console.log("The transaction transfer status " + transactionStatusTransfer.toString());
+  const tokenMintedReceipt = await mintToken(USDT.address, amount);
+  console.log("Token minted: ", tokenMintedReceipt);
+  if (tokenMintedReceipt) {
+    const sendTokensReceipt = await sendTokens(accountId, amount);
+    console.log("Tokens sent: ", sendTokensReceipt);
+  }
 }
 
 async function getClient() {
@@ -59,10 +33,69 @@ async function getClient() {
   return client;
 }
 
-// async function mintToken(tokenId: string, amount: Long) {
-//   if (val == '')
-// }
+async function mintToken(tokenId: string, amount: Long): Promise<boolean> {
+  try {
 
+    const client = await getClient();
+
+    const mintTx = await new TokenMintTransaction()
+      .setTokenId(tokenId)
+      .setAmount(amount)
+      .setMaxTransactionFee(new Hbar(10))
+      .execute(client);
+
+
+    //Request the receipt of the transaction
+    const receipt = await mintTx.getReceipt(client);
+
+    //Get the transaction consensus status
+    const transactionStatus = receipt.status;
+
+    console.log("Transaction: " + mintTx.toString());
+
+    if (transactionStatus.toString() == SUCCESS_MESSAGE) {
+      console.log("Token minted successfully");
+      return true;
+    } else {
+      console.log("Token minted failed");
+      return false;
+    }
+  }
+  catch (error) {
+    console.error("Error during token minting:", error);
+    return false;
+  }
+}
+
+export async function sendTokens(accountId: AccountId, amount: Long): Promise<boolean> {
+  try {
+    console.log("Transfering tokens from: " + SPHERE_WALLET_ADRESS + " to: " + accountId);
+
+    const client = await getClient();
+
+    const transferTx = await new TransferTransaction()
+      .addTokenTransfer(USDT.address, SPHERE_WALLET_ADRESS, -amount)
+      .addTokenTransfer(USDT.address, accountId, amount)
+      .execute(client);
+
+    const receiptTransfer = await transferTx.getReceipt(client);
+
+    const transactionStatusTransfer = receiptTransfer.status;
+
+    console.log("Transaction: " + transferTx.toString());
+
+    if (transactionStatusTransfer.toString() == SUCCESS_MESSAGE) {
+      console.log("Token transfered successfully");
+      return true;
+    } else {
+      console.log("Token transfer failed");
+      return false;
+    }
+  } catch (error) {
+    return false;
+    console.error("Error during token transfer:", error);
+  }
+}
 
 export async function checkTokenSupport() {
   const accountIdString = useWallet.getState().accountId;
@@ -71,7 +104,7 @@ export async function checkTokenSupport() {
   try {
     const accountId = AccountId.fromString(accountIdString) as any;
     if (!accountId) return false;
-    
+
     const signer = hashconnect.getSigner(accountId) as any;
     const transaction = await new TokenAssociateTransaction()
       .setAccountId(accountId)
