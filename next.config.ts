979b5cf9e4c43c847054c429bf9c44a10327c1ea
j <anonymosmail@gmail.com>
2024-12-18 13:00:47 +0100

    hotfix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // transpilePackages: ['hashconnect','@hashgraph/sdk','@hashgraph/hedera-wallet-connect'],
+  swcMinify: false,
 };
 
 export default nextConfig;
